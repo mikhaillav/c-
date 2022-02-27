bcc6aa9610e0f1a1de6e838ed61c16839d7f22f0
mikhaillav <59438110+mikhaillav@users.noreply.github.com>
2022-02-27 16:04:07 +0300

    Add files via upload

---
+++ console/WinApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace WinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(259, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "отправить команду";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 27);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "введте команду:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 36);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "закрыть консоль";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "result:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "result";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Window";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ console/WinApp/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WinApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ProcessStartInfo psi;
+                psi = new ProcessStartInfo("cmd", @$"/k echo off&{textBox1.Text}");
+                Process.Start(psi);
+                label3.Visible = true;
+                label3.Text = "успех!";
+                await Task.Delay(2000);
+                label3.Visible = false;
+
+            }
+            catch
+            {
+                label3.Visible = true;
+                label3.Text = "упсс... ошибка при запуске консоли(";
+                await Task.Delay(2000);
+                label3.Visible = false;
+            }
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("cmd"))
+                {
+                    try
+                    {
+                        proc.Kill();
+                        label3.Visible = true;
+                        label3.Text = "успех!";
+                        await Task.Delay(2000);
+                        label3.Visible = false;
+                        break;
+                    }
+                    catch
+                    {
+                        label3.Visible = true;
+                        label3.Text = "упсс... ошибка при закрытии консоли(";
+                        await Task.Delay(2000);
+                        label3.Visible = false;
+                    }
+                    
+
+                }
+            }
+            catch
+            {
+                label3.Visible = true;
+                label3.Text = "упсс... ошибка при обнаружении процесса консоли(";
+                await Task.Delay(2000);
+                label3.Visible = false;
+            }
+        }
+    }
+}
